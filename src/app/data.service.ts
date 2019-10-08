ad744594685e211a0877aeee72a7c34aeb715836
celfonica <servicioscelfonica@gmail.com>
2019-10-08 16:53:55 -0700

    Http Request API config | Pulling data from reddit json : On home page check console log

---
+++ src/app/data.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  constructor(private http: HttpClient) { }
+
+  getRemoteData() {
+    return this.http.get("https://www.reddit.com/r/gifs/top/.json?limit=10&sort=hot");
+  }
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { DataService } from "../data.service";
 
 @Component({
   selector: 'app-tab1',
@@ -7,5 +8,13 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private dataService: DataService) {}
+
+ngOnInit() {
+
+  this.dataService.getRemoteData().subscribe(data => {
+    console.log("Remote Data:");
+    console.log(data);
+  });
+ }
 }
