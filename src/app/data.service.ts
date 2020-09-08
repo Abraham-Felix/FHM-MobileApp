e78fc92a3a99608e203698bd9bf58edf7859a794
celfonica <servicioscelfonica@gmail.com>
2020-09-07 22:20:25 -0700

    Example | auth in head for api auth get http request

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+//import { Observable, Subject } from 'rxjs';
+//import 'rxjs/add/operator/map';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +10,16 @@ export class DataService {
   constructor(private http: HttpClient) { }
 
   getRemoteData() {
-    return this.http.get("https://fhmnewsletter.herokuapp.com/contacts.json");
+  // example to put auth in head for api request
+  //  let headers = new HttpHeaders();
+  //  this.authToken = this.loadToken();
+  //  headers.append('Authorization', this.authToken);
+  //  headers.append('content-Type','application/json');
+    return this.http.get("https://fintechhorizonsmedia.com/posts.json");
   }
 }
+//get auth token
+//loadToken(){
+//    const Token = localStorage.getItem('id_token');
+//     return Token;
+//  }
