b95251db4231020ecbd2bfc1c8d1145d243dfbcc
celfonica <servicioscelfonica@gmail.com>
2020-10-27 16:28:42 -0700

    Fetchin | FHM api | updt

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -8,15 +8,18 @@ import { DataService } from "../data.service";
 })
 export class Tab1Page {
 
-  id : any[];
-  name : any[];
+  //id : any[];
+  title : any[];
+  author : any[];
+  blog : any[]
+
   constructor(private dataService: DataService) {}
 
 ngOnInit() {
 
   this.dataService.getRemoteData().subscribe(data => {
     console.log("Remote Data:" + JSON.stringify(data));
-   // console.log(data);
+    //console.log(data);
     this.parseJson(data);
   });
  }
@@ -25,14 +28,19 @@ parseJson(data)
 {
    let jsonArray = data;
 
-  this.id = [];
-  this.name = [];
+  //this.id = [];
+  this.title = [];
+  this.author = [];
+  this.blog = [];
 
   for(let i=0; i< jsonArray.length ; i++)
   {
      let jsonObject = jsonArray[i];
-     this.id.push(jsonObject.id);
-     this.name.push(jsonObject.name);
+     //this.id.push(jsonObject.id);
+     this.title.push(jsonObject.title);
+     this.author.push(jsonObject.author.name);
+     this.blog.push(jsonObject.blog_entry);
+
   }
 }
 }
